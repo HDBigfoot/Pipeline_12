62a385a8b66b0aac093b8b8d6e3e9e472badd923
Hanif Djunaedy <hanif.djunaedy@gmail.com>
2025-07-02 11:24:16 +0700

    Edited ThePipeline script and Pipeline12 main script

--- ThePipeline/ThePipeline.sh
+++ ThePipeline/ThePipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 3 --min-coverage 3 --min-freq-for-hom 0.9 --min-var-freq 0.05 > ${STANDARD_PATH}/Called/${i}.snp.vcf
-varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 6 --min-coverage 10 --min-avg-qual 25 --min-strands2 2 --min-var-freq 0.1 >  ${STANDARD}/Called/${i}.vSNPs.vcf
-varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 20 --min-coverage 20 --min-avg-qual 25 --min-strands 2 --min-var-freq 0.9 > ${i}.fSNPs.vcf
+varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 6 --min-coverage 10 --min-avg-qual 25 --min-strands2 2 --min-var-freq 0.1 >  ${STANDARD_PATH}/Called/${i}.vSNPs.vcf
+varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 20 --min-coverage 20 --min-avg-qual 25 --min-strands2 2 --min-var-freq 0.9 > ${STANDARD_PATH}/Called/${i}.fSNPs.vcf
