7b2f24c2974c9dde02c73063b3e65cbe87f894f6
Hanif Djunaedy <hanif.djunaedy@gmail.com>
2025-07-02 15:11:07 +0700

    edited tb_variant_filter

--- Pipeline12_v2.sh
+++ Pipeline12_v2.sh
@@ -15,7 +15,7 @@ for i in $Lines
     varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 3 --min-coverage 3 --min-freq-for-hom 0.9 --min-var-freq 0.05 --output-vcf 1 > ${STANDARD_PATH}/Called/${i}.snp.vcf
     varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 6 --min-coverage 10 --min-avg-qual 25 --min-strands2 2 --min-var-freq 0.1 --output-vcf 1 >  ${STANDARD_PATH}/Called/${i}.vSNPs.vcf
     varscan mpileup2snp ${STANDARD_PATH}/Called/${i}.mpileup --p-value 0.01 --min-reads2 20 --min-coverage 20 --min-avg-qual 25 --min-strands2 2 --min-var-freq 0.9 --output-vcf 1 > ${STANDARD_PATH}/Called/${i}.fSNPs.vcf
-    tb_variant_filter -R pe-ppe ${STANDARD_PATH}/Called/${i}.snp.vcf ${STANDARD_PATH}/Called/${i}.masked.snp.vcf
-    tb_variant_filter -R pe-ppe ${STANDARD_PATH}/Called/${i}.vSNPs.vcf ${STANDARD_PATH}/Called/${i}.masked.vSNPs.vcf
-    tb_variant_filter -R pe-ppe ${STANDARD_PATH}/Called/${i}.fSNPs.vcf ${STANDARD_PATH}/Called/${i}.masked.fSNPs.vcf
+    tb_variant_filter -R pe_ppe ${STANDARD_PATH}/Called/${i}.snp.vcf ${STANDARD_PATH}/Called/${i}.masked.snp.vcf
+    tb_variant_filter -R pe_ppe ${STANDARD_PATH}/Called/${i}.vSNPs.vcf ${STANDARD_PATH}/Called/${i}.masked.vSNPs.vcf
+    tb_variant_filter -R pe_ppe ${STANDARD_PATH}/Called/${i}.fSNPs.vcf ${STANDARD_PATH}/Called/${i}.masked.fSNPs.vcf
     done
